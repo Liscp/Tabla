c460e99911d2089bdce9efdc224b5435689fe07d
Liscp <34662555+Liscp@users.noreply.github.com>
2018-08-06 15:02:06 -0500

    Add files via upload

---
+++ ejemplo cursor.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+CREATE PROCEDURE simple_loop ( ) 
+BEGIN
+  DECLARE counter BIGINT DEFAULT 0;
+  
+  my_loop: LOOP
+    SET counter=counter+1;
+
+    IF counter=10 THEN
+      LEAVE my_loop;
+    END IF;
+
+    SELECT counter;
+
+  END LOOP my_loop;
+END$$
+DELIMITER ;
+
+call simple_loop();
